5e0fe9d010068aa9f7e6ee6700c47af6aae8a837
MadhuSamala <madhusamala.trainer@gmail.com>
2022-09-28 13:00:40 +0100

    constructors-code-added

--- src/com/qa/java/oop/Employee.java
+++ src/com/qa/java/oop/Employee.java
@@ -10,5 +10,26 @@ public class Employee {
 	byte age;
 	char gender;
 	//Each employee object will have its own state
+	
+	//No-arg constructor
+	/*
+	 * Employee(){ System.out.println("Employee() constructor is called.."); id =
+	 * 111; name = "emp1"; department = "development"; salary = 456456.45; age = 25;
+	 * gender = 'M';
+	 * 
+	 * }
+	 */
+	public Employee(int id, String name, String department, double salary, byte age, char gender) {
+		System.out.println("Employee(arg) constructor is called...");
+		this.id = id; // this refers to the current instance (object)
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+		this.age = age;
+		this.gender = gender;
+	}
+	
+	//Arg constructor
+	
 
 }
--- src/com/qa/java/oop/Main.java
+++ src/com/qa/java/oop/Main.java
@@ -6,45 +6,44 @@ public class Main {
 //Creating objects for my Employee class
 		int x;
 		x = 50;
-		
-		Employee employee; // a reference variable		
-		employee = new Employee(); // an employee object is created
-		System.out.println("id: "+ employee.id);
-		System.out.println("name:" +employee.name);
-		System.out.println("department:" +employee.department);
-		System.out.println("gender:" + employee.gender);
-		System.out.println("salary:" + employee.salary);
-		System.out.println("age:" + employee.age);
-		
-		employee.id = 111;
-		employee.name = "emp1";
-		employee.department = "development";
-		employee.gender = 'M';
-		employee.salary = 46564.45;
-		employee.age = 25;
-		
-		System.out.println("id: "+ employee.id);
-		System.out.println("name:" +employee.name);
-		System.out.println("department:" +employee.department);
-		System.out.println("gender:" + employee.gender);
-		System.out.println("salary:" + employee.salary);
-		System.out.println("age:" + employee.age);
-		
-		Employee employee2 = new Employee();
-		employee2.id = 222;
-		employee2.name = "emp2";
-		employee2.department = "QA";
-		employee2.gender = 'F';
-		employee2.salary = 56564.45;
-		employee2.age = 26;
 
-		System.out.println("id: "+ employee2.id);
-		System.out.println("name:" +employee2.name);
-		System.out.println("department:" +employee2.department);
+		Employee employee; // a reference variable
+		employee = new Employee(111, "emp1", "development", 24234.234, (byte) 25, 'M'); // an employee object is created
+
+		System.out.println("id: " + employee.id);
+		System.out.println("name:" + employee.name);
+		System.out.println("department:" + employee.department);
+		System.out.println("gender:" + employee.gender);
+		System.out.println("salary:" + employee.salary);
+		System.out.println("age:" + employee.age);
+
+		/*
+		 * employee.id = 111; employee.name = "emp1"; employee.department =
+		 * "development"; employee.gender = 'M'; employee.salary = 46564.45;
+		 * employee.age = 25;
+		 */
+
+		/*
+		 * System.out.println("id: "+ employee.id); System.out.println("name:"
+		 * +employee.name); System.out.println("department:" +employee.department);
+		 * System.out.println("gender:" + employee.gender); System.out.println("salary:"
+		 * + employee.salary); System.out.println("age:" + employee.age);
+		 */
+
+		Employee employee2 = new Employee(222, "emp2", "QA", 456456.456, (byte) 30, 'F');
+		/*
+		 * employee2.id = 222; employee2.name = "emp2"; employee2.department = "QA";
+		 * employee2.gender = 'F'; employee2.salary = 56564.45; employee2.age = 26;
+		 */
+
+		System.out.println("id: " + employee2.id);
+		System.out.println("name:" + employee2.name);
+		System.out.println("department:" + employee2.department);
 		System.out.println("gender:" + employee2.gender);
 		System.out.println("salary:" + employee2.salary);
 		System.out.println("age:" + employee2.age);
-		
+
+		//Employee emp3 = new Employee();
 	}
 
 }
